0d15ae60fd9eed0c34ab7c5eacd38a3d1472e3ce
Mada Gower <gmgower@gmail.com>
2019-09-27 20:57:51 -0500

    Axios get call.

--- smurfs/src/store/actions/index.js
+++ smurfs/src/store/actions/index.js
@@ -1,7 +1,16 @@
+import axios from 'axios'
+
 export const FETCHING_SMURF_START = 'FETCHING_SMURF_START'
 
 export const getSmurfs = () => {
     return dispatch => {
         dispatch({type: FETCHING_SMURF_START})
+        axios.get('http://localhost:3333/smurfs')
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err.response)
+        })
     }
 }
